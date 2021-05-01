fb7bb78227f6de23391d3cca573a965d5734c7b9
AlphaOrez <76860873+AlphaOrez@users.noreply.github.com>
2021-05-01 13:09:14 +0500

    Create explode.php

---
+++ explode.php
@@ -0,0 +1,9 @@
+<?php
+$string = "Burch Jr, Philip H., The American establishment, Research in political economy 6(1983), 83-156";
+$str_arr = explode (",", $string);
+$a=$str_arr;
+echo "<pre>";
+print_r($a);
+echo "</pre>";
+?>
+
